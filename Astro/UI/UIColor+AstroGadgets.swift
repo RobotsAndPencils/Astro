e812603ffca4b02fc9aea9549aa20246f318b7a8
Brandon Evans <brandon@brandonevans.ca>
2016-02-01 11:29:21 -0700

    Add code documentation for color gadgets

--- Astro/UI/UIColor+AstroGadgets.swift
+++ Astro/UI/UIColor+AstroGadgets.swift
@@ -17,6 +17,14 @@
 import UIKit
 
 extension UIColor {
+    /**
+     Create a color from a string of hexadecimal characters.
+
+     - parameter hexString: A six or eight-character string of hexadecimal (0-9, A-F) characters, optionally preceded with a # character. Every two characters represents the 0-255 value of a RGBA color component. For example, #FF0000 represents the color red, and #FF000099 would be a translucent red.
+     - parameter alpha:     The desired alpha of the created color, from 0.0 to 1.0. If `hexString` contains an alpha component it will be multiplied with this value. Default value is 1.0.
+
+     - returns: A new color created from the parsed values.
+     */
     public convenience init?(hexString: String, alpha: Double = 1.0) {
         var parsedRed: UInt32 = 255
         var parsedGreen: UInt32 = 255
@@ -44,7 +52,12 @@ extension UIColor {
 
         self.init(red: CGFloat(parsedRed) / 255.0, green: CGFloat(parsedGreen) / 255.0, blue: CGFloat(parsedBlue) / 255.0, alpha: (CGFloat(alpha) * CGFloat(parsedAlpha)) / 255.0)
     }
-    
+
+    /**
+     Creates a hexadecimal string representation of a color. For example, UIColor.redColor().hexRGB() will return the string "FF0000".
+
+     - returns: The hex string representation.
+     */
     public func hexRGB() -> String {
         var r: CGFloat = 0
         var g: CGFloat = 0
@@ -53,7 +66,12 @@ extension UIColor {
         assert(getRed(&r, green: &g, blue: &b, alpha: &a), "Unable to get RGB channels from UIColor")
         return NSString(format:"%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255)) as String
     }
-    
+
+    /**
+     Creates a hexadecimal string representation of a color. For example, UIColor.redColor().hexRGBA() will return the string "FF0000FF".
+
+     - returns: The hex string representation.
+     */
     public func hexRGBA() -> String {
         var r: CGFloat = 0
         var g: CGFloat = 0
