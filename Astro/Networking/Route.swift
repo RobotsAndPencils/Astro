73789dc13b5428ab0da85b035cf32eba53876265
Chad Sykes <Chad.Sykes@RobotsAndPencils.com>
2016-09-12 15:18:01 -0600

    Upgrade project to Swift 2.3 using Xcode conversion tool

--- Astro/Networking/Route.swift
+++ Astro/Networking/Route.swift
@@ -35,7 +35,7 @@ public struct Route: URLRequestConvertible {
     }
 
     public var URL: NSURL {
-        return baseURL.URLByAppendingPathComponent(self.path)
+        return baseURL.URLByAppendingPathComponent(self.path)!
     }
 
     // MARK: - URLRequestConvertible
