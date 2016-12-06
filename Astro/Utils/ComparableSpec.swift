19643d154aaf7a79860464d35c4c96b142e52bf3
Brandon Evans <brandon@brandonevans.ca>
2016-12-06 11:56:51 -0700

    Add constrain function in Comparable extension file

---
+++ Astro/Utils/ComparableSpec.swift
@@ -0,0 +1,52 @@
+//  Copyright © 2016 Robots and Pencils, Inc. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//  Neither the name of the Robots and Pencils, Inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+import Quick
+import Nimble
+@testable import Astro
+
+class ComparableSpec: QuickSpec {
+    override func spec() {
+        describe("clamp") {
+            context("x is less than min") {
+                it("returns min") {
+                    let result = clamp(0, min: 5, max: 10)
+                    expect(result).to(equal(5))
+                }
+            }
+            context("x is equal to min") {
+                it("returns min") {
+                    let result = clamp(5, min: 5, max: 10)
+                    expect(result).to(equal(5))
+                }
+            }
+            context("x is between min and max") {
+                it("returns x") {
+                    let result = clamp(5, min: 0, max: 10)
+                    expect(result).to(equal(5))
+                }
+            }
+            context("x is equal to max") {
+                it("returns max") {
+                    let result = clamp(10, min: 5, max: 10)
+                    expect(result).to(equal(10))
+                }
+            }
+            context("x is greater than max") {
+                it("returns max") {
+                    let result = clamp(10, min: 0, max: 5)
+                    expect(result).to(equal(5))
+                }
+            }
+        }
+    }
+}
+
